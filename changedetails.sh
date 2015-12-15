d7531abd21ac78c1c2bd709fc3e6f1ab84cbdd2d
mohi048 <mohit.048@gmail.com>
2015-12-15 19:33:55 +0530

    Adding Files

---
+++ changedetails.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+### General Scripts for UBUNTU OS to change Hostname , IP Address and SSH Root login Permissions ###
+
+
+NewHostName=""
+NewIpAddr="192.168.72.130"
+RootLogin="Yes"
+
+THISHOST=$(hostname)
+ThisIP=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
+
+sed -i "s/$THISHOST/$NewHostName/g" /etc/hosts
+sed -i "s/$THISHOST/$NewHostName/g" /etc/hostname
+sed -i "s/$ThisIP/$NewIpAddr/g" /etc/network/interfaces
+sed -i "/PermitRootLogin/c\PermitRootLogin Yes"  /etc/ssh/sshd_config
+
+echo "New IP Address = $NewIpAddr"
+echo "New Machine Name = $NewHostName"
+echo "Setting Root Login permission to $RootLogin"
+echo "Rebooting the machine , Please standBy !!!!!"
+echo "################ USE THE NEW IP ADDRESS $NewIpAddr TO LOGIN ###############"
+shutdown -r +1
+
+
+
+
